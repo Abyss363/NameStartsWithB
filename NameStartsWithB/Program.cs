ccdecd1fa4bd3b7650e24035df64178bc7e51b55
Abyss363 <fortuneodo@gmail.com>
2024-08-14 11:40:00 +0100

    Add project files.

---
+++ NameStartsWithB/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NameStartsWithB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] orderIds = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            foreach (var orderId in orderIds)
+            {
+                if(orderId.StartsWith("B"))
+                {
+                    Console.WriteLine(orderId);
+                }
+            }
+        }
+    }
+}
